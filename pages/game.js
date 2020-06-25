0b64bdcb74cf9f79c0b880fa72d64a35e4c05cf1
Anthony K <antonyk@users.noreply.github.com>
2020-06-25 14:00:42 -0400

    welcome page

---
+++ pages/game.js
@@ -0,0 +1,11 @@
+
+  const CELL_SIZE = 20;const WIDTH = 800;const HEIGHT = 600;
+
+
+export default function Game() {
+  return (
+    <div>
+      <div className="Board" style={{ width: WIDTH, height: HEIGHT }}></div>
+    </div>
+  )
+}
