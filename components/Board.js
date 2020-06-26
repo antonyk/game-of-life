67035eeafce848dadc282462e32abf0ed062dc17
Anthony K <antonyk@users.noreply.github.com>
2020-06-26 10:49:08 -0400

    functioning simulation in next

--- components/Board.js
+++ components/Board.js
@@ -1,74 +1,136 @@
 import styles from './Board.module.css'
-
-// local state
-// create a 2D matrix/array
-// board has the grid and the controls?
-// let's assume board = grid
-
-import { useState } from "react"
-import deepcopy from 'deepcopy'
+import { useState, useEffect, useRef, useCallback } from "react"
+// import deepcopy from 'deepcopy'
 import produce from 'immer'
 import { Button } from './Button'
 
-function buildGrid(rows, cols) {
-  return Array(rows).fill(Array(cols).fill(0))
+// Experimental; Version 2
+const doubleBuffer = {
+  active: 0,
+  states: []
+}
+function updateBuffer(buffer, newState) {
+  buffer.active = Number(!buffer.active)
+  buffer.states[buffer.active] = newState
+  return buffer
 }
 
-const numRows = 25,
-      numCols = 25
+const defaultSize = 25
+const offsets = [
+  { name: 'nw', row: -1, col: -1 },
+  { name: 'n', row: -1, col: 0 },
+  { name: 'ne', row: -1, col: 1 },
+  { name: 'w', row: 0, col: -1 },
+  { name: 'e', row: 0, col: 1 },
+  { name: 'sw', row: 1, col: -1 },
+  { name: 's', row: 1, col: 0 },
+  { name: 'se', row: 1, col: 1 },
+]
 
-// function toggleCell(i,j) {
+// function getBuffer(buffer) {}
+// function readLiveBuffer(buffer) {}
+// function readPastBuffer(buffer) {}
 
-// }
+function buildGrid(rows, cols, val) {
+  return Array(rows).fill(Array(cols).fill(val))
+}
 
-export default function Board() {
+function getNewGeneration(grid) {
+    // grid is a 2D array[i][j]
+    const newGen = produce(grid, copy => {
+    for (let i = 0; i < grid.length; i++) {
+      for (let j = 0; j < grid[i].length; j++) {
+        let neighborsCount = 0
+        offsets.forEach((item) => {
+          const x = item.row + i
+          const y = item.col + j
+          if (x >= 0 && x < grid.length && y >= 0 && y < grid[i].length) {
+            neighborsCount += grid[x][y]
+          }
+        })
+
+        if (neighborsCount < 2 || neighborsCount > 3) {
+          copy[i][j] = 0
+        }
+        else if (neighborsCount === 3) {
+          copy[i][j] = 1
+        }
+      }
+    }
+  })
+  // console.log(newGrid)
+  return newGen
+
+}
+
+export default function App() {
+  const [duration, setDuration] = useState(500)
+  const [numRows, setNumRows] = useState(defaultSize)
+  const [numCols, setNumCols] = useState(defaultSize)
   const [isRunning, setIsRunning] = useState(false)
+  const [grid, setGrid] = useState(buildGrid(defaultSize, defaultSize, 0))
+  const [generation, setGeneration] = useState(0)
+  // const [buffer, setBuffer] = useState(doubleBuffer)
+  const simStateRef = useRef(isRunning)
+  simStateRef.current = isRunning // fixes sync issue
 
-  const [grid, setGrid] = useState(buildGrid(numRows, numCols))
+  const simulate = useCallback(() => {
+    if (!simStateRef.current) {
+      return;
+    }
+    // run simulation here recursively until stop condition
+    // generate a new grid based on current
+    // wait a while
+    setGrid((curGrid) => {
+      return getNewGeneration(curGrid)
+    })
 
-  // console.log(grid)
+    setTimeout(simulate, duration)
+  }, [])
 
-  // phase 1: re-assign the whole grid on a single, manual toggle
+  function startStopHandler(e) {
+    e.preventDefault()
+    setIsRunning(!isRunning)
+    if (!isRunning) {
+      simStateRef.current = true
+      simulate()
+    }
+  }
+
+  function cellToggleHandler(e, i, j) {
+    e.preventDefault()
+    setGrid(produce(grid, copy => {
+      copy[i][j] = Number(!grid[i][j])
+    }))
+  }
 
   return (
     <>
-    <button
-      onClick={() => setIsRunning(!isRunning)}
-    >
-      {isRunning ? "Stop" : "Start"}
-    </button>
-    <div 
-      className={styles.grid}
-      style={{
-        // display: grid,
-        gridTemplateRows: `repeat(${numRows}, 25px)`,
-        gridTemplateColumns: `repeat(${numCols}, 25px)`,
-      }}
-
-    >
-      {grid.map((row,i) => row.map((cell,j) => {
-          return (
-            <div
-              onClick={() => {
-                const gridBuffer = produce(grid, newGrid => {
-                  newGrid[i][j] = Number(!grid[i][j])
-                })
-                setGrid(gridBuffer)
-              }}
-              key={`${i}${j}`}
-              style={{
-                width: 20,
-                height: 20,
-                backgroundColor: cell ? "green" : "whitesmoke",
-                border: "solid 1px black"
-              }}
-              // className={`${styles.cell}${cell === 1 ? ' '+styles.cell_on : ''}`}
-            >
-            </div>
-          )
-        }))}
-    </div>
+      <button onClick={startStopHandler}>
+        {isRunning ? "STOP" : "START"}
+      </button>
+      <div 
+        className={styles.grid}
+        style={{
+          display: "grid",
+          gridTemplateRows: `repeat(${numRows}, 23px)`,
+          gridTemplateColumns: `repeat(${numCols}, 23px)`
+        }}
+      >
+        {grid.map((row, i) => row.map((col, j) => (
+          <div 
+            onClick={(e) => cellToggleHandler(e, i, j)}
+            // className={`${styles.cell}${cell === 1 ? ' '+styles.cell_on : ''}`}
+            key={`${i}${j}`}
+            style={{
+              width: 20,
+              height: 20,
+              border: "solid 1px black",
+              backgroundColor: col ? "green" : undefined,
+            }}
+          />)))
+        }
+      </div>
     </>
   )
-
 }
