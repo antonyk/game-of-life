4bf05070db63cf6d918bfa8e2022691ce3d15b65
Anthony K <antonyk@users.noreply.github.com>
2021-02-04 19:36:42 -0500

    convert pages to typescript

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,8 @@
 import '../static/styles/globals.css'
+import type { AppProps /*, AppContext */ } from 'next/app'
 import Layout from '../components/layout'
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
--- pages/game.tsx
+++ pages/game.tsx
@@ -1,13 +1,12 @@
-import Board from "../components/Board";
+import Board from '../components/Board'
 
 // const CELL_SIZE = 20;const WIDTH = 800;const HEIGHT = 600;
 
-
 export default function Game() {
   return (
-    <div>
+    <section>
       <Board />
       {/* <div className="Board" style={{ width: WIDTH, height: HEIGHT }}></div> */}
-    </div>
+    </section>
   )
 }
